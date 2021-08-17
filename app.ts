ac7f0f1f7c6cdff7027625d2c0c012440b3e6f8a
goodidea0616 <lixiangde888@gmail.com>
2021-08-17 18:13:57 +0900

    fix issues

--- app.ts
+++ app.ts
@@ -2,16 +2,6 @@ import { GoogleDriveService } from './src/gdrive-api';
 const express = require('express');
 const app = express();
 
-// var sass = require('node-sass-middleware');
-
-// app.use(
-//     sass({
-//         src: __dirname + '/scss', //where the sass files are 
-//         dest: __dirname + '/public', //where css should go
-//         debug: true // obvious
-//     })
-// );
-
 app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
